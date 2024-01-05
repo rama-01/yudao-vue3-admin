3c431eb0e6d0c3bb552941bd17e25c3b1e2056cb
unknown <ramastarzhu01@gmail.com>
2024-01-05 18:05:34 +0800

    husky配置不成功

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
+  <div>
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
+  <div class="w-20 h-20 bg-amber-300">hello world</div>
   <RouterView />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './assets/styles/index.scss'  //无法定位文件夹默认index文件
+import 'element-plus/dist/index.css'  //element plus样式文件，需要全局导入
 
 const app = createApp(App)
 
